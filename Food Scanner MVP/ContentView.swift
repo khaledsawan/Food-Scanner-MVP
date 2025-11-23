4ce3419f2f0b26a79164a38cdb7ddfdf5948286b
khaledsawan <aky2115327@gmail.com>
2025-11-23 03:42:57 +0300

    Initial Commit

---
+++ Food Scanner MVP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Food Scanner MVP
+//
+//  Created by khaled sawan on 23.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Food Scanner MVP/Food_Scanner_MVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_Scanner_MVPApp.swift
+//  Food Scanner MVP
+//
+//  Created by khaled sawan on 23.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Food_Scanner_MVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
